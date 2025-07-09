804aaf06fa60acdab9ffac30d46fe2516feea46f
vitorgabrieldev <vitor@clickweb.com.br>
2025-07-09 12:37:11 -0300

    feat: :rocket:

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className='font-bold text-[14px]'>TESTE</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
